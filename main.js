36331e4c92731fc1d6d1fa67946ab3df1ecf1dfd
donatociao <ciao.donatociao@gmail.com>
2019-05-16 00:57:51 +0200

    Milestone1_ok

--- main.js
+++ main.js
@@ -3,3 +3,11 @@
 // e dall’interlocutore (bianco) assegnando due classi CSS diverse
 // Aggiunta di un messaggio: l’utente scrive un testo nella parte bassa
 // e cliccando invia il testo viene aggiunto al thread sopra, come messaggio verde
+
+$('.invio').click(function() {
+  var new_message = $('.message-container.template').clone();
+  var type = $('.scritto').val();
+  new_message.removeClass('template');
+  new_message.children('.message').html(type);
+  $('.chat-box').append(new_message);
+});
