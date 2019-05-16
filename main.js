50a3de08c615d923cb31f0ce64bb17651b000fde
donatociao <ciao.donatociao@gmail.com>
2019-05-16 15:09:38 +0200

    Milestone2: risposta ok

--- main.js
+++ main.js
@@ -11,3 +11,21 @@ $('.invio').click(function() {
   new_message.children('.message').html(type);
   $('.chat-box').append(new_message);
 });
+
+// // Milestone 2
+// // - Risposta dall’interlocutore: ad ogni inserimento di un messaggio,
+// l’utente riceverà un “ok” come risposta, che apparirà dopo 1 secondo.
+// // - Ricerca utenti: scrivendo qualcosa nell’input a sinistra,
+// vengono visualizzati solo i contatti il cui nome contiene le lettere inserite
+// (es, Marco, Matteo Martina -> Scrivo “mar” rimangono solo Marco e Martina)
+
+function risposta() {
+  var cpu_message = $('.message-container.template').clone();
+  cpu_message.removeClass('template');
+  cpu_message.children('.message').removeClass('utente').html('Ok!');
+  $('.chat-box').append(cpu_message);
+}
+
+$('.invio').click(function() {
+setTimeout(risposta, 1000);
+});
